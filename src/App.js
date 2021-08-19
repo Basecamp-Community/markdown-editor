4e1e970754e4e877734c6749937825fb4be54833
Rishabh Singh <rishabhsinghs90s@gmail.com>
2021-08-19 19:21:32 +0530

    Add React files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactMarkdown from 'react-markdown'
 
 function App() {
+  const [markdown, setMarkdown] = useState("");
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <textarea onChange={handleChange} />
+
+      <div className="preview">
+      <ReactMarkdown children={markdown} />
+      </div>
     </div>
   );
 }
